af5be13de35411108d83687c4712d6233d15f6ac
xrh95@outlook.com <xrh95@outlook.com>
2020-12-09 14:10:45 +0800

    知识点整理

---
+++ vue/vue知识点整理/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <test/>
+</template>
+
+<script>
+import test from './components/test'
+export default {
+  name: 'App',
+  components: {
+    test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
